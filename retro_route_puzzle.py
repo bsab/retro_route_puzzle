43993767048b76ec3ed233e1d5db92d42589d592
sab <tino.saba@gmail.com>
2017-09-15 14:45:07 +0200

    .

--- retro_route_puzzle.py
+++ retro_route_puzzle.py
@@ -10,14 +10,12 @@ directions = ['north', 'south', 'east', 'west']
 
 
 class RetroRoutePuzzle():
-    """La notazione cardinale delle direzioni non ha valore indica esclusivamente che esiste
-    una porta (ad esempio chiamata Nord) che permette di andare dalla stanza X alla stanza Y
-    (ma non viceversa se non diversamente specificato). Questo significa che:
-        Se la stanza X ha porte vero Y W e Z
-        E Z ha porte esclusivamente verso Y e W
-        Da X potrai andare in Z ma da Z non puoi tornare in X
-
-    L'algoritmo del percorso deve permettere la collezione degli oggetti.
+    """A program that will output a valid route one could follow to collect all specified items within a map.
+    The map is a json description of set of rooms with allowed path and contained object.
+    It starts with an input of:
+        json reppresentation of map
+        starting room
+        list of object to collect.
     """
 
     def __init__(self, map_file_name):
@@ -41,19 +39,25 @@ class RetroRoutePuzzle():
         return map_graph
 
     def __set_map_on_graph(self, map_dict):
-        """ creo un grafo orientato (digraph) a  partire dalla mappa json"""
+        """ creo un grafo orientato (digraph) a  partire dalla mappa json.
+        La notazione cardinale delle direzioni non ha valore indica esclusivamente che esiste
+        una porta (ad esempio chiamata Nord) che permette di andare dalla stanza X alla stanza Y
+        (ma non viceversa se non diversamente specificato). Questo significa che:
+         - Se la stanza X ha porte vero Y W e Z
+         - E Z ha porte esclusivamente verso Y e W
+         - Da X potrai andare in Z ma da Z non puoi tornare in X"""
 
         map_graph = None
         try:
             map_graph = nx.DiGraph(name="Map")
 
             for room in map_dict['rooms']:
-                # aggiungo i nodi al grafo
+                # aggiungo i nodi al grafo ...
                 map_graph.add_node(room['id'],
                                    name=room['name'],
                                    objects=room['objects'])
 
-                # leggendo le direzioni, aggiungo li archi fra un nodo e l'altro
+                # e leggendo le direzioni, aggiungo li archi fra un nodo e l'altro
                 for room_dir in directions:
                     if room.has_key(room_dir):
                         next_room=room[room_dir]
@@ -74,55 +78,57 @@ class RetroRoutePuzzle():
             print "MAP GRAPH IS NONE"
         print "---------------------------"
 
-    def __get_row(self, room_id):
-        room_name = self.__map_graph.node[room_id]['name']
-        room_object=None
+    def __collect_room_object(self, room_id):
+        """controllo che nel nodo (ovvero la room) siano presenti oggetti
+        da collezionare, in caso positivo elimino l'oggetto dalla lista
+        objects_to_collect"""
+
+        room_object = None
         if len(self.__map_graph.node[room_id]['objects'])>0:
             room_object = self.__map_graph.node[room_id]['objects'][0]['name']
+            if room_object in objects_to_collect:
+                print '*****popping ', room_object, '...'
+                objects_to_collect.pop(objects_to_collect.index(room_object))
 
-        return room_name, room_object
+        return room_object
 
-    def run_routing(self, start_room_id, objects_to_collect):
+    def navigate_and_collect(self, start_room_id, objects_to_collect):
+        """
+        Seguo tutti i percorsi raggiungibili dal nodo X (start_room_id).
+        Edge classification during
+        Eseguo un Breadth-first search sul directed graph partendo dal nodo X
+        e da questo classifico tutti gli edge ottenuti in una lista.
+        A questo punto, stampo per ogni nodo dell'edge presente nella lista
+        le info del nodo.
+        """
 
         if not self.__map_graph:
-            logger.error('Cannot run algorithm map on graph. The graph is None', exc_info=False)
+            logger.error('Cannot run algorithm on map. The map is None', exc_info=False)
             return False
         try:
-            #seguo tutti i percorsi raggiungibili dal nodo X
-
-            # eseguo una BFS sul grafo inserendo in una lista tutti gli edge
-            # stampo per ogni nodo dell'edge nella lista di cui sopra le info
-            #
             print "ID Room Object collected"
             print "---------------------------"
 
             edge_routing = list(nx.bfs_edges(self.__map_graph, start_room_id))
-            print "edge_routing--->", edge_routing
+            #print "edge_routing--->", edge_routing
             for (room_from, room_to) in edge_routing:
-                #room from
-                room_name, room_object = self.__get_row(room_from)
+
+                room_name = self.__map_graph.node[room_from]['name']
+                room_object = self.__collect_room_object(room_from)
                 print room_from, " ", room_name, " ", room_object
-                if room_object:
-                    #print 'popping ', room_object, '...'
-                    objects_to_collect.pop(objects_to_collect.index(room_object))
-                    if not objects_to_collect:
-                        break;
+                if not objects_to_collect:
+                    break;
 
-                #room to
-                room_name, room_object = self.__get_row(room_to)
+                room_name = self.__map_graph.node[room_to]['name']
+                room_object = self.__collect_room_object(room_to)
                 print room_to, " ", room_name, " ", room_object
-                if room_object:
-                    #print 'popping ', room_object, '...'
-                    objects_to_collect.pop(objects_to_collect.index(room_object))
-                    if not objects_to_collect:
-                        break;
+                if not objects_to_collect:
+                    break;
 
-            print "AFTER ID Room Object collected"
-            print objects_to_collect
             print "---------------------------"
 
         except Exception as e:
-            logger.error('Cannot run algorithm map on graph', exc_info=False)
+            logger.error('Cannot run algorithm map on graph', exc_info=True)
             return False
 
 if __name__ == "__main__":
@@ -137,7 +143,10 @@ if __name__ == "__main__":
     # eseguo l'algoritmo
     start_room_id=4
     objects_to_collect=['Knife', 'Potted Plant', 'Pillow']
-    #print objects_to_collect
 
-    retro_route_puzzle.run_routing(start_room_id, objects_to_collect)
+    retro_route_puzzle.navigate_and_collect(start_room_id, objects_to_collect)
+
+    print "Room Object remain to collect:"
+    print objects_to_collect
+
 
